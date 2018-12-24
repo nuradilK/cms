7210f8e5ea17053a99d9c073694ff654c0a59e0c
Rauan Omarov <rauan.omarov@nu.edu.kz>
2018-12-24 23:23:02 +0600

    1. Added contests app 2. Added Contest model 3. Added the first view

--- mycms/settings.py
+++ mycms/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'contests.apps.ContestsConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Almaty'
 
 USE_I18N = True
 
