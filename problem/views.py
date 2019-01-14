984872edda8dd776cbb634b5eac0ae0626ca3d1d
nuradil <mr.nuradil@gmail.com>
2019-01-14 22:47:19 +0600

    bugs fixed

--- problem/views.py
+++ problem/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Problem
 
 def problem_page(request, pk):
-    statement = Problem.objects.get(problem_id=pk).statement
+    statement = Problem.objects.get(problem_id=pk).statement_set.first()
     return render(request, 'problem/problem.html', {'statement':statement})
