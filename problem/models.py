11f9596b048d3d6fc69d398c633bb58077686d1a
nuradilk <mr.nuradil@gmail.com>
2019-03-06 21:08:14 +0600

    add checker

--- problem/models.py
+++ problem/models.py
@@ -5,6 +5,7 @@ import time
 import hashlib
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib import admin
 
 class Problem(models.Model):
     problem_id = models.IntegerField(default=0)
@@ -12,8 +13,13 @@ class Problem(models.Model):
     secret = models.CharField(max_length=100)
     testset_name = models.CharField(max_length=100, default="tests")
     contest = models.ManyToManyField(Contest, blank=True)
+    checker = models.TextField(blank=True)
     def __str__(self):
         return str(self.problem_id)
+
+
+class ProblemAdmin(admin.ModelAdmin):
+    fields = ('problem_id', 'key', 'secret', 'testset_name', 'contest')
 
 
 class Test(models.Model):
@@ -52,6 +58,7 @@ def get_statement(params):
 
     return requests.get(api_url + method, params).json()
 
+
 def get_info(params, instance, Time):
     api_url = "https://polygon.codeforces.com/api/"
     method = 'problem.info'
@@ -61,16 +68,41 @@ def get_info(params, instance, Time):
             instance.problem_id) + '&time=' + Time + '#' + instance.secret).encode('utf-8')).hexdigest();
     return requests.get(api_url + method, params).json()
 
+
 def get_test(params, instance, Time):
     api_url = "https://polygon.codeforces.com/api/"
     method = 'problem.tests'
 
-    params['apiSig'] = 'ajkoi4' + hashlib.sha512(str(
-        'ajkoi4/problem.tests?apiKey=' + instance.key + '&problemId=' + str(
-            instance.problem_id) + '&testset=' + instance.testset_name + '&time=' + Time + '#' + instance.secret).encode(
-        'utf-8')).hexdigest()
+    params['apiSig'] = 'ajkoi4' + \
+        hashlib.sha512(str('ajkoi4/problem.tests?apiKey=' + instance.key + '&problemId=' + str(instance.problem_id) + \
+            '&testset=' + instance.testset_name + '&time=' + \
+                Time + '#' + instance.secret).encode('utf-8')).hexdigest()
     params['testset'] = instance.testset_name
     return requests.get(api_url + method, params).json()
+
+def get_name (params, instance, Time):
+    api_url = "https://polygon.codeforces.com/api/"
+    method = 'problem.checker'
+
+    params['apiSig'] = 'nuradi' + \
+        hashlib.sha512(str('nuradi/' + method + '?apiKey=' + instance.key + \
+            '&problemId=' + str(instance.problem_id) + \
+                '&time=' + Time + '#' + instance.secret).encode('utf-8')).hexdigest()
+    del params['testset']
+    return requests.get(api_url + method, params).json()['result']
+
+
+def get_file(params, instance, Time, name):
+    api_url = "https://polygon.codeforces.com/api/"
+    method = 'problem.viewFile'
+
+    params['apiSig'] = 'nuradi' + \
+        hashlib.sha512(str('nuradi/problem.viewFile?apiKey=' + instance.key + \
+            '&name=' + name  + '&problemId=' + str(instance.problem_id) + \
+                '&time=' + Time + '&type=source#' + instance.secret).encode('utf-8')).hexdigest()
+    params['name'] = name
+    params['type'] = 'source'
+    return requests.get(api_url + method, params).content
 
 @receiver(post_save, sender=Problem)
 def get_problem_data(sender, instance, created, **kwargs):
@@ -78,16 +110,20 @@ def get_problem_data(sender, instance, created, **kwargs):
     params = {
         'apiKey': instance.key,
         'time': Time,
-        'apiSig': '654321' + hashlib.sha512(str(
-            '654321/problem.statements?apiKey=' + instance.key + '&problemId=' + str(
-                instance.problem_id) + '&time=' + Time + '#' + instance.secret).encode('utf-8')).hexdigest(),
+        'apiSig': '654321' + \
+            hashlib.sha512(str('654321/problem.statements?apiKey=' + \
+                instance.key + '&problemId=' + \
+                    str(instance.problem_id) + '&time=' + Time + '#' + \
+                        instance.secret).encode('utf-8')).hexdigest(),
         'problemId': instance.problem_id,
     }
 
     statement = get_statement (params)
     info = get_info(params, instance, Time)
     tests = get_test(params, instance, Time)
-
+    checker_name = get_name(params, instance, Time)
+    checker = get_file(params, instance, Time, checker_name)
+    print (checker)
 
     # print(tests)
     for i in tests['result']:
