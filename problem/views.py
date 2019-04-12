d5f8d99146df6ec748467bbd1bde92c2c297eb35
Krauchman <rauan.omarov@nu.edu.kz>
2019-04-13 00:21:29 +0600

    Fixed problem_page view

--- problem/views.py
+++ problem/views.py
@@ -5,7 +5,7 @@ from contest.models import Contest
 
 def problem_page(request, contest_pk, problem_pk):
     contest = get_object_or_404(Contest, pk=contest_pk)
-    problem = get_object_or_404(Problem, problem_id=problem_pk, contest=contest)
+    problem = get_object_or_404(Problem, problem_id=problem_pk, contests=contest)
 
     context = {
         'statement': problem.statement,
